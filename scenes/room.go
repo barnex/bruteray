b95ecc249a1613ab3b575c8d44f60cb156bf11b7
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2017-10-05 22:42:33 +0200

    render a room

--- scenes/room.go
+++ scenes/room.go
@@ -13,17 +13,26 @@ func main() {
 	black := Shiny(WHITE.EV(-6), EV(-3))
 
 	e.Add(
-		Or(Box(Vec{0, 0, 0}, 3, 3, 3, wall), Box(Vec{2.8, 0, 2}, 1, 3, 1.2, wall)),
+		SurfaceAnd(
+			Box(Vec{0, 0, 0}, 3, 3, 4, wall),
+			Inverse(Or(
+				Box(Vec{3, 0, 1.7}, 1, 2.2, .5, wall),
+				Box(Vec{3, 1.3, 0}, 1, .7, .5, wall),
+			)),
+		),
 		Sheet(Ey, 0, Checkboard(0.75, white, black)),
 	)
 
 	e.AddLight(
-		SphereLight(Vec{1, 2.1, 1}, .07, WHITE.EV(5)),
+		SphereLight(Vec{6, 2, 1}, 1, WHITE.EV(10)),
 	)
 
-	e.Camera = Camera(0.9).Transl(1, 1.7, -0).RotScene(0 * Deg).Transf(RotX4(0 * Deg))
+	e.SetAmbient(Flat(WHITE.EV(-4)))
+
+	e.Camera = Camera(0.8).Transl(0, 1.7, -3).RotScene(-30 * Deg).Transf(RotX4(0 * Deg))
 	e.Camera.AA = true
 	e.Recursion = 5
 	e.Cutoff = EV(5)
 
+	serve.Env(e)
 }
