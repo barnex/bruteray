3e048b36ef498d8d7f02ba619984e68437e9a0ef
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2018-03-03 13:41:32 +0100

    godoc2md experiment

---
+++ raster/standard.go
@@ -0,0 +1,11 @@
+package raster
+
+import (
+	"fmt"
+
+	"github.com/barnex/bruteray/br"
+)
+
+func Standard(e *br.Env) {
+	fmt.Println("![fig](shots/062.jpg)")
+}
---
+++ shape/sphere_test.go
@@ -0,0 +1,16 @@
+package shape
+
+import (
+	"github.com/barnex/bruteray/br"
+	"github.com/barnex/bruteray/mat"
+	"github.com/barnex/bruteray/raster"
+)
+
+func ExampleSphere() {
+	e := br.NewEnv()
+	e.Add(NSphere(1, mat.ShadeShape(br.RED)))
+	raster.Standard(e)
+
+	//Output:
+	//![fig](shots/062.jpg)
+}
