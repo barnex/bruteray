8dc915116bf45201588c7f31deef8c9839635f67
Arne Vansteenkiste <barnex@gmail.com>
2019-02-24 15:11:56 +0000

    tweak slides

--- scenes/slides/slide.go
+++ scenes/slides/slide.go
@@ -26,12 +26,12 @@ func main() {
 		Rect(Vec{0, 10, 0}, Ey, 150, U, 100, Diffuse(tex)),
 		NewSheet(Ey, -0, Checkboard(30, Diffuse(WHITE.EV(-.6)), Diffuse(WHITE.EV(-1)))),
 
-		NewSphere(120, Reflective(WHITE.EV(-1))).Transl(Vec{-40, 60, 140}),
+		NewSphere(120, ReflectFresnel(3, BLACK)).Transl(Vec{-40, 60, 140}),
 		NewSphere(100, Refractive(1, 1.5)).Transl(Vec{120, 0, -120}),
 
 		NewSphere(60, Diffuse(Color{.2, .2, .8})).Transl(Vec{-230, 30, 100}),
 		NewSphere(60, Shiny(Color{.2, .8, .2}, 0.1)).Transl(Vec{-190, 30, 0}),
-		NewSphere(60, Shiny(Color{.8, .2, .2}, 0.1)).Transl(Vec{-190, 30, 180}),
+		NewSphere(60, Shiny(Color{.8, .2, .2}, 0.2)).Transl(Vec{-190, 30, 190}),
 
 		NewCylinder(Y, Vec{170, 30, 145}, 100, 80, Shiny(WHITE.EV(-1), 0.2)),
 		NewCube(80, Diffuse(WHITE.EV(-1))).Transl(Vec{220, 20, 0}),
@@ -55,8 +55,9 @@ func main() {
 	e.Recursion = 5
 	e.Cutoff = EV(2)
 
-	cam.Focus = 300
+	cam.Focus = 280
 	cam.Aperture = 15
+	cam.Diaphragm = DiaHex
 
 	serve.Env(cam, e)
 }
